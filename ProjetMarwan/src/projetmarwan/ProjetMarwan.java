b02465c674d659696c8588f82db701b8541bc3af
MarwanGLT <ton-email-github@gmail.com>
2026-03-24 15:49:52 +0100

    First commit

--- ProjetMarwan/src/projetmarwan/ProjetMarwan.java
+++ ProjetMarwan/src/projetmarwan/ProjetMarwan.java
@@ -14,7 +14,7 @@ public class ProjetMarwan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Marwan lE riF");
     }
     
 }
